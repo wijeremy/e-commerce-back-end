5b2886be99c8d5ff0639154cc8e6eed4daa22de7
wijeremy <jeremydavwilliams@gmail.com>
2021-08-01 15:57:55 -0500

    well, i hope i'm not missing somehting. looks like i didn't have to do much to product-routes.js, just add both gets. on to tag-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productByID = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!productByID) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productByID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
