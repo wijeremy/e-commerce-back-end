bc460b1123ef6218d71c3014c39b530b894d4b24
wijeremy <jeremydavwilliams@gmail.com>
2021-08-01 16:33:04 -0500

    forgot an async hahaha, okay NOW it should be good to go

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,7 +111,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const deletedProduct = await Product.destroy({
